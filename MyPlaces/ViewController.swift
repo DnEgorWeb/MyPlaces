e145bcd4a6282bb78dd773e8e1b7463afa1280ea
Egor Deryabin <ederyabin@sfxdx.ru>
2019-08-22 11:29:41 +0700

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Egor Deryabin on 22/8/2562 BE.
+//  Copyright © 2562 Egor Deryabin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
